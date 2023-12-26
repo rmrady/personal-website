768bf31e49a25e16021e27d0d304e1c94fd3ee0c
razieh7 <rmrady758@gmail.com>
2023-12-26 22:32:05 +0330

    tree commit

--- index.js
+++ index.js
@@ -1,8 +1,34 @@
 AOS.init();
 
-let x = document.getElementsByTagName('main')[0]
-let count_scroll = 0;
-x.addEventListener('scroll', (e) => {
-    count_scroll = (Math.round(e.target.scrollTop));
-    console.log(count_scroll);
+
+
+let box_img = document.querySelectorAll('.box-img>div')
+const hight = []
+box_img.forEach((val)=>{
+    hight.push(val.offsetTop)
 })
+
+
+
+let x= document.getElementsByTagName('main')[0]
+x.addEventListener('scroll', function(){
+    let st = (x.scrollTop)+500
+    if(st > hight[1] && st < hight[12]){
+        document.getElementsByClassName('_p')[0].classList.add('fixed')
+    }else{
+        document.getElementsByClassName('_p')[0].classList.remove('fixed')
+    }
+
+    box_img.forEach((val,i)=>{
+
+        if(st > hight[i] && st < hight[i+1]){
+            val.classList.add('opacity-50')
+        }else{
+            box_img[i].classList.remove('opacity-50')
+        }
+    })
+
+    
+    
+})
+
